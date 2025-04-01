40e40d94da00ca15c5a1459d3f6ac850e89ea795
Hina <hina@example.com>
2025-04-01 16:35:15 +0200

    магія починається ✨

---
+++ bot.py
@@ -0,0 +1,14 @@
+import os
+from telegram import Update
+from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
+
+TOKEN = os.getenv("T8087039975:AAHilkGMZAIwQtglfaeApBHDpcNREqlpCNE")
+
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await update.message.reply_text("Прив ку! Я бот Хіни 🌸")
+
+app = ApplicationBuilder().token('T8087039975:AAHilkGMZAIwQtglfaeApBHDpcNREqlpCNE').build()
+app.add_handler(CommandHandler("start", start))
+
+print("Бот запущено 🐾")
+app.run_polling()
