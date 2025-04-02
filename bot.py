1ef39b1a00d27a6515aac15b789c0f954949b4b1
Hina <hina@example.com>
2025-04-02 14:05:11 +0200

    додала /todo з видаленням, датами та автоочищенням 💡

--- bot.py
+++ bot.py
@@ -44,6 +44,10 @@ def gendered(name, gender):
     else:
         return name or "зайчик"
 
+# ---------- КОМАНДА /start ----------
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await update.message.reply_text("Прив ку! Обери щось ⤵️", reply_markup=reply_markup)
+
 # ---------- TODO ----------
 async def todo(update: Update, context: ContextTypes.DEFAULT_TYPE):
     user_id = update.message.from_user.id
